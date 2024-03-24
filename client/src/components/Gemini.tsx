4902e5ebe9ae0519d0e0c939a6788f9bd59c06e3
Melanie <mptan18@gmail.com>
2024-03-24 09:23:54 -0400

    Update Gemini.tsx

--- client/src/components/Gemini.tsx
+++ client/src/components/Gemini.tsx
@@ -1,75 +1,115 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-import React, {useState} from "react"
-import { render } from "react-dom"
-import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Gemini() {
+  const [docUploaded, setDocUploaded] = useState(false);
+  const [pdfFile, setPdfFile] = useState(new Uint8Array());
+  const [responseText, setResponseText] = useState("");
 
-    
-    const [docUploaded, setDocUploaded] = useState(false)
-    const [pdfFile, setPdfFile] = useState(new Uint8Array())
+  async function queryGemini(data: string) {
+    const apiKey = import.meta.env.VITE_GEMINI_API;
+    const gemini = new GoogleGenerativeAI(apiKey);
+    const model = gemini.getGenerativeModel({ model: "gemini-pro" });
 
-    async function queryGemini(data: string) {
-        const apiKey = import.meta.env.VITE_GEMINI_API
-        const gemini = new GoogleGenerativeAI(apiKey)
-        const model = gemini.getGenerativeModel({ model: "gemini-pro"})
+    // console.log(data)
+    // console.log(apiKey)
 
-        // console.log(data)
-        // console.log(apiKey)
+    const prompt = "Please review my resume for me: " + data;
+    // const prompt = data
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    // console.log(response.text());
+    setResponseText(response.text());
+    console.log(responseText);
+    console.log("Completed");
+  }
 
-        const prompt = "Please review my resume for me: " + data
-        // const prompt = data
-        const result = await model.generateContent(prompt)
-        const response = await result.response
-        console.log(response.text())
-        console.log("Completed")
-
+  async function parseData(event: React.FormEvent) {
+    event.preventDefault();
+    const file = (document.getElementById("res-sub") as HTMLInputElement)
+      .files?.[0];
+    console.log(file);
+    if (!file) {
+      alert("Please select a file to upload");
+      return;
     }
-    
-    async function parseData(event: React.FormEvent) {
-        event.preventDefault()
-        const file = (document.getElementById('res-sub') as HTMLInputElement).files?.[0]
-        console.log(file)
-        if (!file) {
-            alert('Please select a file to upload')
-            return
+    if (file?.type == "text/plain") {
+      const data = await file.text();
+      console.log(data);
+      await queryGemini(data);
+    } else {
+      const buf = await file.arrayBuffer();
+      const data = new Uint8Array(buf);
+      setPdfFile(data);
+      setDocUploaded(true);
+      //await queryGemini(file, false)
+    }
+    // await queryGemini(data)
+  }
+
+  async function handleDrop(event: React.DragEvent) {
+    console.log("file dropped");
+    event.preventDefault();
+    if (event.dataTransfer.items) {
+      [...event.dataTransfer.items].forEach((item,i) => {
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`file[${i}].name = ${file.name}`);
         }
-        if(file?.type == "text/plain") {
-            const data = await file.text()
-            console.log(data)
-            await queryGemini(data)
-        } else {
-            
-            const buf = await file.arrayBuffer()
-            const data = new Uint8Array(buf)
-            setPdfFile(data)
-            setDocUploaded(true)            
-            //await queryGemini(file, false)
-        }     
-        // await queryGemini(data)
+      });
+    } else {
+      [...event.dataTransfer.files].forEach((file,i) => {
+        console.log(`file[${i}].name = ${file.name}`);
+      });
     }
+  }
 
-    return <>
-        <form onSubmit={parseData}>
-            <label htmlFor="resume-submit">Upload your resume:</label>
-            <input type="file" className="resume-submit" id="res-sub" name="resume-submit" accept=".txt, .pdf" />
-            <button type="submit">Submit</button>
-        </form>
-        {docUploaded ?<>
-        <Document file={{data: pdfFile}}>
-                <Page pageNumber={1} onLoadSuccess={async (page) => {
-            console.log('SUCCESS LOAD')
-            // console.log(page.getTextContent())
-            var textObj = await page.getTextContent()
-            var text = textObj.items.map((s) => s.str).join("")
-            // console.log(text)
-            await queryGemini(text)
-            }}/>
-            </Document>
-        
+  async function handleDragOver(event: React.DragEvent) {
+    console.log("File in the drop zone");
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <div 
+        id="drop_zone" 
+        onDrop={(handleDrop)} 
+        onDragOver={handleDragOver} 
+        style={{ border: "2px dashed #aaa", padding: "20px", borderRadius: "1rem" }}>
+        <p>Drag and drop your resume as a .pdf or .txt</p>
+      </div>
+      <form onSubmit={parseData}>
+        <label htmlFor="resume-submit">or upload your resume: </label>
+        <input
+          type="file"
+          className="resume-submit"
+          id="res-sub"
+          name="resume-submit"
+          accept=".txt, .pdf"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {docUploaded ? (
+        <>
+          <Document file={{ data: pdfFile }}>
+            <Page
+              pageNumber={1}
+              onLoadSuccess={async (page) => {
+                console.log("SUCCESS LOAD");
+                // console.log(page.getTextContent())
+                var textObj = await page.getTextContent();
+                var text = textObj.items.map((s) => s.str).join("");
+                // console.log(text)
+                await queryGemini(text);
+              }}
+            />
+          </Document>
+          {{responseText}}
         </>
-            : null}
-            
-        </>
+      ) : null}
+    </>
+  );
 }
