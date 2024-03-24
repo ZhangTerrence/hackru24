a2d9ce075aa1cbb6cf5bfd8b3a57d16df8d7923c
Evan Yang <71720197+Evany226@users.noreply.github.com>
2024-03-23 20:18:02 -0400

    merge complete

--- client/src/components/Gemini.tsx
+++ client/src/components/Gemini.tsx
@@ -1,23 +1,20 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-import React, {useState} from "react"
-import { render } from "react-dom"
-import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Gemini() {
-  async function queryGemini(data: string | File, text: boolean) {
+  const [docUploaded, setDocUploaded] = useState(false);
+  const [pdfFile, setPdfFile] = useState(new Uint8Array());
+
+  async function queryGemini(data: string) {
     const apiKey = import.meta.env.VITE_GEMINI_API;
     const gemini = new GoogleGenerativeAI(apiKey);
     const model = gemini.getGenerativeModel({ model: "gemini-pro" });
 
-    
-    const [docUploaded, setDocUploaded] = useState(false)
-    const [pdfFile, setPdfFile] = useState(new Uint8Array())
-
-    async function queryGemini(data: string) {
-        const apiKey = import.meta.env.VITE_GEMINI_API
-        const gemini = new GoogleGenerativeAI(apiKey)
-        const model = gemini.getGenerativeModel({ model: "gemini-pro"})
+    // console.log(data)
+    // console.log(apiKey)
 
     const prompt = "Please review my resume for me: " + data;
     // const prompt = data
@@ -36,52 +33,50 @@ export default function Gemini() {
       alert("Please select a file to upload");
       return;
     }
-    
-    async function parseData(event: React.FormEvent) {
-        event.preventDefault()
-        const file = (document.getElementById('res-sub') as HTMLInputElement).files?.[0]
-        console.log(file)
-        if (!file) {
-            alert('Please select a file to upload')
-            return
-        }
-        if(file?.type == "text/plain") {
-            const data = await file.text()
-            console.log(data)
-            await queryGemini(data)
-        } else {
-            
-            const buf = await file.arrayBuffer()
-            const data = new Uint8Array(buf)
-            setPdfFile(data)
-            setDocUploaded(true)            
-            //await queryGemini(file, false)
-        }     
-        // await queryGemini(data)
+    if (file?.type == "text/plain") {
+      const data = await file.text();
+      console.log(data);
+      await queryGemini(data);
+    } else {
+      const buf = await file.arrayBuffer();
+      const data = new Uint8Array(buf);
+      setPdfFile(data);
+      setDocUploaded(true);
+      //await queryGemini(file, false)
     }
     // await queryGemini(data)
   }
 
-    return <>
-        <form onSubmit={parseData}>
-            <label htmlFor="resume-submit">Upload your resume:</label>
-            <input type="file" className="resume-submit" id="res-sub" name="resume-submit" accept=".txt, .pdf" />
-            <button type="submit">Submit</button>
-        </form>
-        {docUploaded ?<>
-        <Document file={{data: pdfFile}}>
-                <Page pageNumber={1} onLoadSuccess={async (page) => {
-            console.log('SUCCESS LOAD')
-            // console.log(page.getTextContent())
-            var textObj = await page.getTextContent()
-            var text = textObj.items.map((s) => s.str).join("")
-            // console.log(text)
-            await queryGemini(text)
-            }}/>
-            </Document>
-        
+  return (
+    <>
+      <form onSubmit={parseData}>
+        <label htmlFor="resume-submit">Upload your resume:</label>
+        <input
+          type="file"
+          className="resume-submit"
+          id="res-sub"
+          name="resume-submit"
+          accept=".txt, .pdf"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {docUploaded ? (
+        <>
+          <Document file={{ data: pdfFile }}>
+            <Page
+              pageNumber={1}
+              onLoadSuccess={async (page) => {
+                console.log("SUCCESS LOAD");
+                // console.log(page.getTextContent())
+                var textObj = await page.getTextContent();
+                var text = textObj.items.map((s) => s.str).join("");
+                // console.log(text)
+                await queryGemini(text);
+              }}
+            />
+          </Document>
         </>
-            : null}
-            
-        </>
+      ) : null}
+    </>
+  );
 }
--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -1,9 +1,11 @@
 import "../css/Upload.css";
+import Gemini from "../components/Gemini";
 
 export const Upload = () => {
   return (
     <>
       <section className="upload-wrapper">
+        <Gemini />
         <p className="upload-main-text">Upload resume</p>
       </section>
     </>
